ebceb9921c3e4b67b7a54b34dc88b3d116833ea5
Darragh Connolly <darraghconnolly@Darraghs-MBP.lan>
2025-05-22 13:19:00 +0100

    redis removed from settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,13 +135,3 @@ SPOTIFY_URL = "https://api.spotify.com/v1/me"
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
-
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1"), 
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#         }
-#     }
-# }
